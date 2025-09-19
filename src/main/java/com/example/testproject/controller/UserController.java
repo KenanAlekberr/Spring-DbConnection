337252696876478fc4508e3c2a9a26350846abe6
Kənan Ələkbərov <kananha@code.edu.az>
2025-09-19 19:09:58 +0400

    Deleted unnecessary import

--- src/main/java/com/example/testproject/controller/UserController.java
+++ src/main/java/com/example/testproject/controller/UserController.java
@@ -3,10 +3,14 @@ package com.example.testproject.controller;
 import com.example.testproject.dto.request.UserRequest;
 import com.example.testproject.dto.response.UserResponse;
 import com.example.testproject.service.UserService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
